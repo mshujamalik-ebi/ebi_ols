47941f6e91d8d9d69d5bb27688dd99cdb300ba0c
Liang <shenzhuxi@gmail.com>
2016-04-15 15:22:02 +0100

    save

---
+++ ebi_ols.js
@@ -0,0 +1,51 @@
+(function ($) {
+
+/**
+ * Attaches the autocomplete behavior to all required fields.
+ */
+Drupal.behaviors.ebi_ols = {
+  attach: function (context) {
+    var $context = $(context);
+    $context.find('a.ebi_ols_formatter_default').each(function(index, value){
+      var URL = $(this).attr('ols_url');
+      var item = $(this);
+      $.ajax({
+        url : URL,
+        success : function(data) {
+          item.text(data.label);
+        },
+        dataType : 'json',
+      });
+    });
+
+    $.ui.autocomplete.prototype._renderItem = function (ul, item) {
+      return $("<li></li>").data("item.autocomplete", item).append("<a>" + item.label + "</a>").appendTo(ul);
+    };
+    var URL_PREFIX = "http://www.ebi.ac.uk/ols/beta/api/select?ontology=";
+    $context.find('input.ols-autocomplete').autocomplete ({
+      source: function (request, response) {
+        var ontology = this.element.attr('ontology');
+        var URL = URL_PREFIX + this.element.attr('ontology') + "&rows=1000&q=" + this.element.val();
+        $.ajax({
+          url : URL,
+          success : function(data) {
+            var suggestions = [];
+            $.each(data.highlighting, function(key, value) {
+              if (value.label_autosuggest) {
+                var label = value.label_autosuggest.join();
+                if (value.synonym_autosuggest) {
+                  label = label + ' (' + value.synonym_autosuggest.join() + ')';
+                }
+                suggestions.push({value: key, label: label});
+              }
+            });
+            response(suggestions);
+          },
+          dataType : 'json',
+        });
+      }
+    });
+  }
+};
+
+})(jQuery);
